505e8ef1236a0e1b0ce73ae3fa66bac47561b84a
krikchaip <krikchai.p@gmail.com>
2019-12-27 23:43:21 +0700

    chore(2nd-challenge): init

---
+++ 2nd-challenge/src/App.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+
+import App from './App'
+
+test('renders hello world', () => {
+  const { getByText } = render(<App />)
+  const linkElement = getByText(/hello world/i)
+
+  expect(linkElement).toBeInTheDocument()
+})
