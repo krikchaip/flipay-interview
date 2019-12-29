b9a5a6443adca86b1da233f803d8b3b2f0f6dcb1
krikchaip <krikchai.p@gmail.com>
2019-12-29 21:59:31 +0700

    test(2nd-chellenge): tree-parser #fromString add 2 more validation cases

--- 2nd-challenge/src/lib/tree-parser.spec.ts
+++ 2nd-challenge/src/lib/tree-parser.spec.ts
@@ -29,9 +29,9 @@ describe('fromString', () => {
     expect(parser.fromString(correctInput)).toEqual<TreeByLevel>(tree)
   })
 
-  describe('return null for any other input', () => {
+  describe('return null when', () => {
     // ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON
-    it('JSON primitives', () => {
+    it('passing JSON primitives', () => {
       expect(parser.fromString('null')).toBeNull()
       expect(parser.fromString('undefined')).toBeNull()
       expect(parser.fromString('false')).toBeNull()
@@ -39,6 +39,84 @@ describe('fromString', () => {
       expect(parser.fromString('"wow"')).toBeNull()
       expect(parser.fromString('{ "a": 3 }')).toBeNull()
       expect(parser.fromString('["a", "b", "c"]')).toBeNull()
+    })
+
+    it('node.level not matching its group', () => {
+      const notMatching = `{
+        "0": [
+          {
+            "id": 10,
+            "title": "House",
+            "level": 30,
+            "children": [],
+            "parent_id": null
+          }
+        ]
+      }`
+
+      expect(parser.fromString(notMatching)).toBeNull()
+    })
+
+    xit('some root level nodes containing a valid parent_id', () => {
+      const validParent = `{
+        "0": [
+          {
+            "id": 10,
+            "title": "House",
+            "level": 0,
+            "children": [],
+            "parent_id": 3
+          },
+          {
+            "id": 20,
+            "title": "Hotel",
+            "level": 0,
+            "children": [],
+            "parent_id": 7
+          },
+          {
+            "id": 30,
+            "title": "Apartment",
+            "level": 0,
+            "children": [],
+            "parent_id": null
+          }
+        ]
+      }`
+
+      expect(parser.fromString(validParent)).toBeNull()
+    })
+
+    xit('some nodes missing parent_id field', () => {
+      const missingParent = `{
+        "1": [
+          {
+            "id": 11,
+            "title": "A",
+            "level": 1,
+            "children": [],
+            "parent_id": null
+          }
+        ],
+        "2": [
+          {
+            "id": 21,
+            "title": "B",
+            "level": 2,
+            "children": [],
+            "parent_id": 17
+          },
+          {
+            "id": 22,
+            "title": "C",
+            "level": 2,
+            "children": [],
+            "parent_id": 17
+          }
+        ]
+      }`
+
+      expect(parser.fromString(missingParent)).toBeNull()
     })
   })
 })
